882d42f324fcf46c14197535659e0c48be0ccca5
irsp33-dev-3986 <134375035+irsp33-dev-3986@users.noreply.github.com>
2023-05-24 12:01:57 +0200

    Add project files.

---
+++ c/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <windows.h>
+
+char team_server_item;
+char team_server_array;
+
+int main(void)
+{
+
+}
+
+struct team_server {
+	int id;
+	int watermark;
+	int sleeptime;
+	char fqdn;
+	char spawnto;
+	char type;
+	char ip;
+};
+
+char build_team_server(int id, int watermark, int sleeptime, char fqdn, char spawnto, char type, char ip)
+{
+	return team_server_item;
+}
+
+char build_team_server_array(char team_server_item)
+{
+	return team_server_array;
+}
